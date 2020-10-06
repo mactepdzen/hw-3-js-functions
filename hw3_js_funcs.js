6423295f7348f88f9517c435d3e1581b25b986cd
Dmitrii Kushnerev <mactepdzen@gmail.com>
2020-10-06 17:26:02 +0300

    add 2 options for implementing the toCamelCase function

--- hw3_js_funcs.js
+++ hw3_js_funcs.js
@@ -1,5 +1,4 @@
 //1. splitAndMerge function
-
 function splitAndMerge(str, sp) {
     var words = str.split(' '); // splitting a string into words
 
@@ -13,8 +12,45 @@ function splitAndMerge(str, sp) {
         result.push(letter.join(sp));
     }
 
-    return(result.join(' '));
+    return result.join(' ');
 }
 
 // ---------------------
 
+//2. convert function
+
+// ---------------------
+
+//3. toCamelCase function
+function toCamelCase(string) {
+    var arr = string.split(''); // array for splitted into chars string
+    var result = []; // array where we put our chars according to the task rules before merging it
+
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i].includes('-') || arr[i].includes('_')){
+            result.push(arr[i + 1].toUpperCase());
+            i++; // if we met '-' or '_' we have to skip next char to avoid repeating it
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result.join('');
+}
+
+// just added implementation with spaces between words in the final output :)
+function toCamelCaseModified(string) {
+    var arr = string.split('');
+    var result = [];
+
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i].includes('-') || arr[i].includes('_')){
+            result.push(` ${arr[i + 1].toUpperCase()}`);
+            i++;
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result.join('');
+}
